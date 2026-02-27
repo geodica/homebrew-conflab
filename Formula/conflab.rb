876bd44acb1cc953ee1094d6ee5175c88386df6b
Matthew Sinclair <matthew.sinclair@gmail.com>
2026-02-27 00:04:42 +0000

    Update conflab to 0.1.1

--- Formula/conflab.rb
+++ Formula/conflab.rb
@@ -1,19 +1,19 @@
 class Conflab < Formula
   desc "CLI and daemon for Conflab agentic collaboration"
   homepage "https://conflab.space"
-  version "0.1.0"
+  version "0.1.1"
   license :cannot_represent
 
-  RELEASE_VERSION = "0.1.0"
+  RELEASE_VERSION = "0.1.1"
 
   on_macos do
     on_arm do
       url "https://github.com/geodica/conflab-dist/releases/download/v#{RELEASE_VERSION}/conflab-aarch64-apple-darwin"
-      sha256 "e2b996eee33db5cd33508ea0f2b94b14d42ddfc4558af9bb0c00a08574097588"
+      sha256 "589d0fddacfb92f339323c6881bd1697d9d164ac354f7d76bc3121cc5c65d36c"
 
       resource "conflabd" do
         url "https://github.com/geodica/conflab-dist/releases/download/v#{RELEASE_VERSION}/conflabd-aarch64-apple-darwin"
-        sha256 "853ac7f44007b7e24857244818317265e49534dd65ff7ea71b5f72b0ef49d0d1"
+        sha256 "716f8768090134526756f4875d9dc564408be4c290dff3887a9f0ccddc084197"
       end
     end
   end
